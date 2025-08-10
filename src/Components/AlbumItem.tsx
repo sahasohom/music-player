ae6a1b138054e3068a01db46515f725393ad04bd
sahasohom <sohomsaha2022@gmail.com>
2025-08-10 22:30:37 +0530

    Minor Changes

--- src/Components/AlbumItem.tsx
+++ src/Components/AlbumItem.tsx
@@ -6,7 +6,7 @@ const AlbumItem = ({ image, name, desc, id }: AlbumItemPropType) => {
   const navigate = useNavigate();
   return (
     <div
-      onClick={() => navigate( `/album/${id}`)}
+      onClick={() => navigate( `/music-player/album/${id}`)}
       className="min-w-[180px] p-2 px-3 rounded cursor-pointer hover:bg-[#ffffff26]"
     >
       <img src={image} alt="image" />
